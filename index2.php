e15f250e923377a0c7c3b0dce98af1d62fc9d0d2
durkeedy <durkeedy@mail.gvsu.edu>
2018-04-15 20:42:43 -0400

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,114 +1,119 @@
+<head>
+<script src="https://cis371a.hopto.org:9040/demo/Chart.bundle.js"></script>
+<script src="https://cis371a.hopto.org:9040/demo/utils.js"></script>
+<link rel="stylesheet" type="text/css" href="main.css"> 
+</head>
 <?PHP
-//var_dump($_POST);
-/*
-$url = $_POST['launch_presentation_return_url'];
-$query=parse_url($url,PHP_URL_QUERY);
-parse_str($query, $out);
-//$course_id=$out['course_id'];
-$course_title=$_POST['context_title'];
-$course_batchUID=$_POST['context_label'];
-$user_id=$_POST['user_id'];
-$name=$_POST['lis_person_name_full'];
-//replacement for the Constant public $HOSTNAME =
-preg_match_all('/\//', $url,$matches, PREG_OFFSET_CAPTURE);  
-$clientURL = substr($url, 0, $matches[0][2][1]);
-*/
-//echo "Welcome $name.  Your course is $course_title ($course_batchUID).<br /><br /><br />";
-
-//$course_id=$_GET['course_id'];
+require('nav.php');
 $clientURL="http://bb.dataii.com:8080";
-
 require_once('classes/Rest.class.php');
 require_once('classes/Token.class.php');
 $rest = new Rest($clientURL);
 $token = new Token();
 $token = $rest->authorize();
-
 $access_token = $token->access_token;
-//$course = $rest->readCourse($access_token, $course_id);
-//var_dump($course);
-//echo $user_id;
-
-
 $user_id = '_45_1';
-
-
 $user = $rest->readUser($access_token, $user_id);
 $c = $user->results;
-
-echo "user is hardcoded, user_id:" .  $user_id;
-echo " <br /> ";
-
+//echo "user is hardcoded, user_id:" .  $user_id;
+//echo " <br /> ";
 $courseList=array();
-foreach($c as $row) {
+echo "<table class='table' >";
+echo "<tr><th>Name</th><th>Description</th></tr>";
 
+foreach($c as $row) {
+		
 	//echo $row->courseId;
 	array_push($courseList,$row->courseId);
 	$course = $rest->readCourse($access_token, $row->courseId);
-        echo "course id: " . $row->courseId . "   name of course: " . $course->name . "<br />";
-        //$membership = $rest->readMembership($access_token, $course_id);
-}
+       //echo "course id: " . $row->courseId . "   name of course: " . $course->name . "<br />";
+       //$membership = $rest->readMembership($access_token, $course_id);
 
+    	echo "<tr>";
+       echo "<td>$course->name</td>";
+       echo "<td>$course->description</td>";
+       
+	echo "</tr>";
+}
+echo "</table>";
 $usersList = array();
 for($i=0; $i<count($courseList);$i++) {
 $membership = $rest->readMembership($access_token,$courseList[$i] );
 $c = $membership->results;
-echo "Users in course " . $courseList[$i] . ":<br /> ";
+//echo "Users in course " . $courseList[$i] . ":<br /> ";
 foreach($c as $row) {
-	echo $row->userId;
+	//echo $row->userId;
 	array_push($usersList,$row->userId);
-
 }
-echo " <br /> ";
+//echo " <br /> ";
 }
-
 $countarray = array_count_values($usersList);
-
 foreach($countarray as $x => $x_value){
-        echo "Student: " . $x . ", Classes together: " . $x_value;
-        echo "<br>";
+        //echo "Student: " . $x . ", Classes together: " . $x_value;
+        //echo "<br>";
 }
-
-
-
-foreach($c as $row) {
-//	echo $row->userId; ;
-
-}
-
-//var_dump($membership);
-//die();
-/*
-$columns = $rest->readGradebookColumns($access_token, $course_id);
-$c=$columns->results;
-//print_r($c);
-foreach($c as $row)
-{
-        // echo $row->name;
-	if($row->name == "Weighted Total") {
-//	echo "another col";
-}
-	//if ($row->externalGrade == 1)
-        if ($row->name == "Weighted Total")
-        {
-         $finalGradeName=$row->name;
-         $finalGradeID=$row->id;
-         //$finalPossible=$row->score->possible;
-         break;
-        }
-
-}
-
-//echo $finalGradeName . " " . $finalGradeID;
-$grades = $rest->readGradebookGrades($access_token, $course_id, $finalGradeID);
-$g=$grades->results;
-
-foreach($g as $row)
-{
-	
-       // echo " User ID:" . $row->userId . " has " .$row->score." out of ".$finalPossible . " points.";
-	
-}
-*/
 ?>
+<?PHP
+$bararray = array_fill(1, 6, 0);
+foreach($countarray as $x => $x_value){
+	if($x_value == 1){
+		$bararray[1]++;
+	}
+	else if($x_value == 2){
+		$bararray[2]++;
+	}
+	else if($x_value == 3){
+		$bararray[3]++;
+	}
+	else if($x_value == 4){
+		$bararray[4]++;
+	}
+	else if($x_value == 5){
+		$bararray[5]++;
+	}
+	else if($x_value == 6){
+		$bararray[6]++;
+	}
+}
+?>
+<div id="canvas-holder" style="width:40%">
+        <canvas id="chart-area" />
+    </div>
+<script>
+    var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100);
+    };
 
+    var config = {
+        type: 'bar',
+        data: {
+            datasets: [{
+                data: [
+			<?PHP
+			for($i = 1; $i < 7; $i++){
+				echo $bararray[$i] . ",";
+			}
+			?>
+             ],
+                backgroundColor: window.chartColors.red,
+                label: 'Dataset 1'
+				}],				
+            labels: [
+                "Shares 1 class",
+                "Shares 2 classes",
+                "3",
+                "4",
+				"5",
+				"6"
+            ]
+        },
+        options: {
+            responsive: true
+        }
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("chart-area").getContext("2d");
+        window.myBar = new Chart(ctx, config);
+    };
+</script>
