67637cf346ca9acbfbbe510b41b11e315ec370eb
durkeedy <durkeedy@mail.gvsu.edu>
2018-04-15 20:43:48 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">Classes</a></li>
+  <li><a href="#contact">Matches</a></li>
+  <li style="float:right"><a href="#about">Logout</a></li>
+</ul>
